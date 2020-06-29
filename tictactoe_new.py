158f68365378c9218f07a6a373bc9428f1a4e93f
karolwk <krl@o2.pl>
2020-06-29 18:51:02 +0200

    added level "easy"

--- tictactoe_new.py
+++ tictactoe_new.py
@@ -1,98 +1,82 @@
-
-board = []
-cells = ""
-
-def make_board(temp: str):
-    # Making starting board with empty lines "_"
-    global board, cells
-    #board = [["_" for x in range(3)] for i in range(3)]
-    cells = temp
-
-    n = 0
-    for i in range(3):
-        board.append([])
-        for x in temp[n: n + 3]:
-            board[i].append(x)
-        n += 3
-    copy_board = [x[:] for x in board]
-    # shift board
-    for i in range(3):
-        for y in range(3):
-            board[y][2-i] = copy_board[i][y]
+import random
 
 
+class TicTacToe:
 
+    def __init__(self):
+        self.board = [["_" for x in range(3)] for i in range(3)]
+        self.player = "X"
 
-def check_win() -> bool:
-    # Checking win or draw condition for hard coded situations
-    # Return 'False' if there are empty lines to fill and 'True' in case of win/draw
-    win_group = ["".join(board[0][:3]), "".join(board[1][:3]), "".join(board[2][:3]),
-                 board[0][0] + board[1][0] + board[2][0],
-                 board[0][1] + board[1][1] + board[2][1],
-                 board[0][2] + board[1][2] + board[2][2],
-                 board[0][0] + board[1][1] + board[2][2],
-                 board[0][2] + board[1][1] + board[2][0],
-                 ]
-    for x in win_group:
-        if x.count("X") == 3 or x.count("O") == 3:
-            print(f"{x[0]} wins")
+    def _check_field(self, cords) -> bool:
+        if self.board[int(cords[0]) - 1][int(cords[1]) - 1] in ("X", "O"):
             return True
-    if all([x.count("_") == 0 for x in win_group]):
-        print("Draw")
-        return True
-
-    return False
-
-
-def print_board():
-    # Print board accordly to instructions
-    print(f"""{9 * "-"}
-| {board[0][2]} {board[1][2]} {board[2][2]} |
-| {board[0][1]} {board[1][1]} {board[2][1]} |
-| {board[0][0]} {board[1][0]} {board[2][0]} |
-{9 * "-"}""")
-#     print(f"""{9 * "-"}
-# | {board[0][0]} {board[0][1]} {board[0][2]} |
-# | {board[1][0]} {board[1][1]} {board[1][2]} |
-# | {board[2][0]} {board[2][1]} {board[2][2]} |
-# {9 * "-"}""")
-
-
-def play():
-    player = "X"
-    if cells.count("X") > cells.count("O"):
-        player = "O"
-    print_board()
-    while True:
-        if not check_win():
-            cords = input("Enter the coordinates: >").split()
-            if cords[0].isdigit() and cords[1].isdigit():
-                if (int(cords[0]) > 3) or (int(cords[1]) > 3):
-                    print("Coordinates should be from 1 to 3!")
-                elif not (board[int(cords[0]) - 1][int(cords[1]) - 1] in ("X", "O")):
-                    if player == "X":
-                        board[int(cords[0]) - 1][int(cords[1]) - 1] = player
-                        player = "O"
-                        print_board()
-                    else:
-                        board[int(cords[0]) - 1][int(cords[1]) - 1] = player
-                        player = "X"
-                        print_board()
-                    if check_win():
-                        break
-                    else:
-                        print("Game not finished")
-                    break
-                else:
-                    print("This cell is occupied! Choose another one!")
-            else:
-                print("You should enter numbers!")
         else:
-            break
+            return False
+
+    def _ai_play(self, difficulty: str) -> None:
+        if difficulty == "easy":
+            self.player = "O"
+            while True:
+                move = [random.randrange(0, 3), random.randrange(0, 3)]
+                if not self._check_field(move):
+                    self.board[int(move[0]) - 1][int(move[1]) - 1] = self.player
+                    self.player = "X"
+                    print('Making move level "easy"')
+                    self._print_board()
+                    break
+
+    def _check_win(self) -> bool:
+        # Checking win or draw condition for hard coded situations
+        # Return 'False' if there are empty lines to fill and 'True' in case of win/draw
+        win_group = ["".join(self.board[0][:3]), "".join(self.board[1][:3]), "".join(self.board[2][:3]),
+                     self.board[0][0] + self.board[1][0] + self.board[2][0],
+                     self.board[0][1] + self.board[1][1] + self.board[2][1],
+                     self.board[0][2] + self.board[1][2] + self.board[2][2],
+                     self.board[0][0] + self.board[1][1] + self.board[2][2],
+                     self.board[0][2] + self.board[1][1] + self.board[2][0],
+                     ]
+        for x in win_group:
+            if x.count("X") == 3 or x.count("O") == 3:
+                print(f"{x[0]} wins")
+                return True
+        if all([x.count("_") == 0 for x in win_group]):
+            print("Draw")
+            return True
+        return False
+
+    def _print_board(self):
+        # Print board accordly to instructions
+        print(f"""{9 * "-"}
+| {self.board[0][2]} {self.board[1][2]} {self.board[2][2]} |
+| {self.board[0][1]} {self.board[1][1]} {self.board[2][1]} |
+| {self.board[0][0]} {self.board[1][0]} {self.board[2][0]} |   
+{9 * "-"}""")
+
+    def play(self):
+        self._print_board()
+        while True:
+            if not self._check_win():
+                cords = input("Enter the coordinates: >").split()
+                if cords[0].isdigit() and cords[1].isdigit():
+                    if (int(cords[0]) > 3) or (int(cords[1]) > 3):
+                        print("Coordinates should be from 1 to 3!")
+                    elif not self._check_field(cords):
+                        self.board[int(cords[0]) - 1][int(cords[1]) - 1] = self.player
+                        self._print_board()
+                        if not self._check_win():
+                            self._ai_play("easy")
+                        else:
+
+                            break
+
+                    else:
+                        print("This cell is occupied! Choose another one!")
+                else:
+                    print("You should enter numbers!")
+            else:
+                break
 
 
-cells = input("Eneter cells: >")
+game = TicTacToe()
+game.play()
 
-make_board(cells)
-print(board)
-play()
